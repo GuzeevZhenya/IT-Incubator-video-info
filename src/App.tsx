12c32d5aa6e46f505471f2172a53b6ba64ae0bf5
GuzeevZhenya <shyst.evgeny@mail.ru>
2024-02-15 16:47:11 +0300

    feat:add router-dom code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./components/Site.module.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { PageOne } from "./components/pages/PageOne";
+import { PageTwo } from "./components/pages/PageTwo";
+import { PageThree } from "./components/pages/PageThree";
+import { Error404 } from "./components/pages/Error404";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>
+          <div>
+            <NavLink
+              to={"/adidas"}
+              className={(isActive) => {
+                return isActive.isActive ? styles.active : styles.navLink;
+              }}
+            >
+              page1
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to={"/adidas1"}
+              className={(isActive) => {
+                return isActive.isActive ? styles.active : styles.navLink;
+              }}
+            >
+              page2
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to={"/adidas2"}
+              className={(isActive) => {
+                return isActive.isActive ? styles.active : styles.navLink;
+              }}
+            >
+              page3
+            </NavLink>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/adidas"} />} />
+            <Route path="/adidas" element={<PageOne />} />
+            <Route path="/adidas1" element={<PageTwo />} />
+            <Route path="/adidas2" element={<PageThree />} />
+
+            <Route path="adidas/error404" element={<Error404 />} />
+            <Route path="*" element={<Navigate to="adidas/error404" />} />
+          </Routes>
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
     </div>
   );
 }
